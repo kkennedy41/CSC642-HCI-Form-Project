8d8ef4d382961f06ae5610f5619094edb4f274a3
kkennedy41 <56007720+kkennedy41@users.noreply.github.com>
2020-07-20 14:54:25 -0700

    added bootstrap

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from flaskext.mysql import MySQL
 from flask_wtf import FlaskForm
 from wtforms import Form, BooleanField, StringField, PasswordField, SelectField, DateField, validators
 from wtforms.validators import *
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 from models.forms import DataSurveyForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey1'
+app.config['GOOGLEMAPS_KEY'] = ''
+GoogleMaps(app, key)
+Bootstrap(app)
 
 #------------ database connection -------------------
 app.config['MYSQL_HOST'] = 'localhost'
@@ -25,10 +30,24 @@ cursor = conn.cursor()
 #------------ index page ----------------------------
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  #theForm = DataSurveyForm(request.form)
-  #theForm.firstName = firstName
-    #else: #do this if error in form
-      #return "~ERROR~ not a valid form"
+  theForm = DataSurveyForm(request.form)  #create object
+
+  if request.method == 'POST' and theForm.validate_on_submit():
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    street = request.form['streetaddress']
+    city = request.form['city']
+    stateUSA = request.form['stateUSA']
+    zipcode = request.form['zipcode']
+    feet = request.form['feet']
+    inches = request.form['inches']
+    day = request.form['day']
+    month = request.form['month']
+    year = request.form['year']
+    edu = request.form['education']
+    phone = request.form['phone']
+    email = request.form['email']
+
 
   #do this if 'GET' request
   return render_template('index.html')
@@ -38,30 +57,14 @@ def index():
 #------------ verification endpoint -----------------
 @app.route('/verification', methods=['GET', 'POST'])
 def verification():
-  
-
-
-  # firstname = request.form['firstname']
-  # lastname = request.form['lastname']
-  # street = request.form['streetaddress']
-  # city = request.form['city']
-  # stateUSA = request.form['stateUSA']
-  # zip = request.form['zip']
-  # feet = request.form['feet']
-  # inches = request.form['inches']
-  # day = request.form['day']
-  # month = request.form['month']
-  # year = request.form['year']
-  # edu = request.form['education']
-  # phone = request.form['phone']
-  # email = request.form['email']
-
-  return render_template('verification.html') #left is what html reads and sends to {{formInfo}}, right is what py reads
+  return_data = print()
+  return render_template('verification.html', data=return_data)  #left is what html reads and sends to {{formInfo}}, right is what py reads
 #---------------------------------------------------
 
 
 #--------------- main fxn --------------------------
 if __name__ == "__main__":
   app.secret_key = 'secretkey1'
+  app.goo
   app.run(debug=True)
   #app.run(host='0.0.0.0', port='80', debug=True)
--- models/forms.py
+++ models/forms.py
@@ -4,76 +4,97 @@ from wtforms.validators import *
 
 
 class DataSurveyForm(FlaskForm):
+  """model for each form instance submitted"""
 
   #----- name -----
-  firstName = StringField('first_name',
-                          validators= [ 
+  firstName = StringField('First name: ',
+                          validators = [ 
                             DataRequired(message='Please enter your first name'),
-                            Length(max=20)
+                            Length(max=20, message='Max. length 20 characters')
                           ])
-  lastName = StringField('last_name',
+
+  lastName = StringField('Last name: ',
                           validators = [
                             DataRequired(message='Please enter your last name'),
                             Length(max=20)
                           ])
 
   #--- address ----
-  streetAddress = StringField('street_address',
-                              validators=[DataRequired(),
-                              Length(max=40)
-                            ])
-  city = StringField('city_label',
+  streetAddress = StringField('Street address: ',
+                              validators = [
+                                DataRequired(message='Please enter your street address'),
+                                Length(max=40, message='Max. length 40 characters')
+                              ])
+
+  city = StringField('City: ',
                       validators = [
                         DataRequired(message='Please enter your city'),
-                        Length(max=40)
+                        Length(max=40, message='Max. length 40 characters')
                       ])
-  state = SelectField(validators= [ 
-                      DataRequired(message='Please enter your state')
-                      ])
-  zipcode = StringField('zip',
+
+  stateUSA = SelectField('State: ',
+                        validators= [
+                          DataRequired(message='Please enter your state')
+                        ])
+
+  zipcode = StringField('Zip code: ',
                         validators = [
                           DataRequired(message='Please enter your zip code'),
-                          Length(min=5,max=5),
-                          NumberRange(min=0,max=99999)])
+                          Length(min=5,max=5, message='Please enter a valid 5-digit zip code'),
+                          NumberRange(min=00000,max=99999, message='Please enter a valid 5-digit zip code')
+                        ])
 
   #----bday ------
-  birthday = DateTimeField("birthday", format='%m/%d/%y', validators=[DataRequired(message='Please enter your date of birth')])
+  birthday = DateTimeField("Date of birth: ",
+                          format='%m/%d/%y',
+                          validators = [
+                            DataRequired(message='Please enter your date of birth')
+                          ])
 
   #---- height ----
-  feet = StringField('feet_label', validators=[NumberRange(min=2,max=15)])
-  inches = StringField('inches_label', validators=[NumberRange(min=0,max=11)])
+  feet = StringField('feet_label',
+                    validators = [ 
+                      NumberRange(min=2,max=15, message='Please enter a valid height')
+                    ])
+  inches = StringField('inches_label',
+                      validators = [
+                        NumberRange(min=0,max=11, message='Please enter a valid height')
+                      ])
 
   #----edu --------
-  edulevel = SelectField(validators=[])
+  edulevel = SelectField('Education level:', validators=[])
 
   #----contact info----
-  phone = StringField('phone_label', 
+  phone = StringField('Phone: ', 
                       validators = [
                         DataRequired(message='Please enter a phone number'),
                         Length(min=7, max=7, message='Do not your include area code'),
                         NumberRange(min=0,max=9999999, message='Please enter a valid phone number')
                       ])
 
-  email = StringField('email_address',
+  email = StringField('Email: ',
                       validators= [ 
                         DataRequired(message='Please enter a valid email address'),
-                        Length(min=6, max=35),
+                        Length(min=6, max=35, message='Please enter a valid email addres (max 35 characters)'),
                         Email('Please enter a valid email address')
                       ])
 
   confirmEmail = StringField('confirm_email',
-                            validators=[DataRequired(message='Please confirm your email address'),
-                            EqualTo(email, message='Emails do not match')
-                          ])
-
+                              validators = [
+                                DataRequired(message='Please confirm your email address'),
+                                EqualTo(email, message='Emails do not match')
+                              ])
+  
+  #---- pre submit checks ----
   iAgree = BooleanField('i_accept',
-                        validators=[DataRequired(message='Please check the box to agree')
+                        validators = [ 
+                          DataRequired(message='Please check the box to agree')
                         ])
 
   capcha = StringField('capcha_label',
-                        validators=[DataRequired(message='Please enter the required text'),
-                        EqualTo('not bot', message='Please enter the required text')
-                      ])
+                        validators = [ 
+                          DataRequired(message='Please enter the required text'),
+                          EqualTo('not bot', message='Please enter the required text')
+                        ])
 
   submit = SubmitField()
-
